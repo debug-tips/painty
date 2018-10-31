6d8905058d0542680a869b48312fa7cb8687791b
jasonslyvia <jasonslyvia@gmail.com>
2018-10-31 14:26:24 +0800

    fix: add stack limit and weigeted load

--- index.js
+++ index.js
@@ -6,6 +6,11 @@
 var timing = require('timing2');
 var calculateFMP = require('./calculateFMP');
 
+/* global __PAINTY_STACK_LIMIT__ */
+
+// record at most 100 DOM changes
+var STACK_LIMIT = typeof __PAINTY_STACK_LIMIT__ === 'number' ? __PAINTY_STACK_LIMIT__ : 100;
+
 function now() {
   return performance && performance.now ? performance.now() : Date.now();
 }
@@ -19,7 +24,6 @@ module.exports = function painty(timeout, callback) {
     callback = timeout;
   }
 
-  var start = timing.getEntriesByType('navigation')[0].startTime;
   var records = [];
 
   function logDOMChange() {
@@ -29,6 +33,10 @@ module.exports = function painty(timeout, callback) {
           t: now(),
           domCnt: document.getElementsByTagName('*').length,
         });
+
+        if (records.length === STACK_LIMIT) {
+          done();
+        }
       });
       observer.observe(document, {
         childList: true,
@@ -43,7 +51,12 @@ module.exports = function painty(timeout, callback) {
         t: now(),
         domCnt: document.getElementsByTagName('*').length,
       });
-      logDOMChange();
+
+      if (records.length === STACK_LIMIT) {
+        done();
+      } else {
+        logDOMChange();
+      }
     }, 200);
 
     return function() {
@@ -54,10 +67,20 @@ module.exports = function painty(timeout, callback) {
   var stopLogging = logDOMChange();
   function done() {
     stopLogging();
+
+    var navTimings = timing.getEntriesByType('navigation');
+    if (!navTimings || !navTimings.length) {
+      return;
+    }
+
+    var navTiming = navTimings[0];
+    var start = navTiming.startTime;
+
     callback(calculateFMP({
       records: records,
       start: start,
       timeout: timeout,
+      load: navTiming.duration,
     }));
   }
 
